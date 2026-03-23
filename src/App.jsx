2ec84b6a0295220ec0f5926001e24bc95cc9912c
londonparisbrussels <londonparisbrussels@gmail.com>
2026-03-23 16:03:55 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
-import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import AppLayout from '@/components/layout/AppLayout';
 import Dashboard from '@/pages/Dashboard';
 import Learn from '@/pages/Learn';
